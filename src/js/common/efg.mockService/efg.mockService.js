a4ab78f91df7acfdd2f7d896ec5cfb7a7fc0d7ff
Dominik Schreiber <domischreib@gmail.com>
2014-12-15 19:08:37 +0100

    added api design for /api/v1/event; added lorempixel.com images

--- src/js/common/efg.mockService/efg.mockService.js
+++ src/js/common/efg.mockService/efg.mockService.js
@@ -5,14 +5,13 @@ angular.module('efg.mockService', [
 .factory('mock', function($q, $log, $window) {
 	var mock = {
 		'/api/v1/group': [
-			{id: 'family', title: 'Familie', subtitle: '0 bis 6 Jahre', img: 'glyphicon glyphicon-user'},
-			{id: 'kids', title: 'Kinder', subtitle: '7 bis 12 Jahre', img: 'glyphicon glyphicon-user'},
-			{id: 'teens', title: 'Teens', subtitle: '13 bis 16 Jahre', img: 'glyphicon glyphicon-user'},
-			{id: 'youth', title: 'Jugendliche', subtitle: '17 bis 21 Jahre', img: 'glyphicon glyphicon-user'},
-			{id: 'young-adults', title: 'Junge Erwachsene', subtitle: '22 bis 35 Jahre', img: 'glyphicon glyphicon-user'},
-			{id: 'women', title: 'Frauen', subtitle: 'Frauen', img: 'glyphicon glyphicon-user'},
-			{id: 'men', title: 'Männer', subtitle: 'Männer', img: 'glyphicon glyphicon-user'},
-			{id: 'seniors', title: 'Senioren', subtitle: 'über 60 Jahre', img: 'glyphicon glyphicon-user'}
+			{id: 'family', title: 'Kinderbetreuung', subtitle: '0 bis 6 Jahre', img: 'glyphicon glyphicon-user'},
+			{id: 'kids', title: 'Kindergottesdienst', subtitle: '7 bis 12 Jahre', img: 'glyphicon glyphicon-user'},
+			{id: 'teens', title: 'Gemeindeunterricht', subtitle: '13 bis 16 Jahre', img: 'glyphicon glyphicon-user'},
+			{id: 'youth', title: 'Teenie-Hauskreise', subtitle: '17 bis 21 Jahre', img: 'glyphicon glyphicon-user'},
+			{id: 'young-adults', title: 'unlimited', subtitle: '22 bis 35 Jahre', img: 'glyphicon glyphicon-user'},
+			{id: 'women', title: 'Café Life', subtitle: 'Frauen', img: 'glyphicon glyphicon-user'},
+			{id: 'seniors', title: 'Seniorenkreis', subtitle: 'über 60 Jahre', img: 'glyphicon glyphicon-user'}
 		],
 		'/api/v1/member?fields=name,duties,img':  [
 			{
@@ -22,7 +21,7 @@ angular.module('efg.mockService', [
 					familyname: 'Schmitt'
 				},
 				duties: ['Gemeindeleiter', 'Ältester'],
-				img: 'glyphicon glyphicon-user'
+				img: '//lorempixel.com/500/500/people/7'
 			}, {
 				id: 'thomas',
 				name: {
@@ -30,7 +29,7 @@ angular.module('efg.mockService', [
 					familyname: 'Weber'
 				},
 				duties: ['Ältester'],
-				img: 'glyphicon glyphicon-user'
+				img: '//lorempixel.com/500/500/people/8'
 			}, {
 				id: 'denise',
 				name: {
@@ -38,7 +37,7 @@ angular.module('efg.mockService', [
 					familyname: 'Schmutz'
 				},
 				duties: ['Raumbuchung', 'Kranke'],
-				img: 'glyphicon glyphicon-user'
+				img: '//lorempixel.com/500/500/people/3'
 			}, {
 				id: 'andreas',
 				name: {
@@ -46,7 +45,7 @@ angular.module('efg.mockService', [
 					familyname: 'Petry'
 				},
 				duties: ['Lehre', 'Gebet'],
-				img: 'glyphicon glyphicon-user'
+				img: '//lorempixel.com/500/500/people/9'
 			}, {
 				id: 'daniel',
 				name: {
@@ -54,7 +53,7 @@ angular.module('efg.mockService', [
 					familyname: 'Mertins'
 				},
 				duties: ['Gottesdienst'],
-				img: 'glyphicon glyphicon-user'
+				img: '//lorempixel.com/500/500/people/10'
 			}, {
 				id: 'jonas',
 				name: {
@@ -62,7 +61,7 @@ angular.module('efg.mockService', [
 					familyname: 'Weber'
 				},
 				duties: ['Jugendarbeit', 'Hauskreise'],
-				img: 'glyphicon glyphicon-user'
+				img: '//lorempixel.com/500/500/people/6'
 			}
 		],
 		'/api/v1/member/wilfried?fields=name,duties,description,poster': {
@@ -73,7 +72,7 @@ angular.module('efg.mockService', [
 			},
 			duties: ['Gemeindeleiter', 'Ältester'],
 			description: 'Seit der Gemeindegründung dabei. Der Hirte.',
-			poster: 'http://lorempixel.com/' + $window.innerWidth + '/' + $window.innerHeight + '/people'
+			poster: '//lorempixel.com/1920/1080/people/7'
 		},
 		'/api/v1/member/thomas?fields=name,duties,description,poster': {
 			id: 'thomas',
@@ -83,7 +82,7 @@ angular.module('efg.mockService', [
 			},
 			duties: ['Ältester'],
 			description: 'Seit 2 Jahren Ältester. Der Macher.',
-			poster: 'http://lorempixel.com/' + $window.innerWidth + '/' + $window.innerHeight + '/people'
+			poster: '//lorempixel.com/1920/1080/people/8'
 		},
 		'/api/v1/member/denise?fields=name,duties,description,poster': {
 			id: 'denise',
@@ -93,7 +92,7 @@ angular.module('efg.mockService', [
 			},
 			duties: ['Raumbuchung', 'Kranke'],
 			description: 'lorem ipsum dolor sit amet',
-			poster: 'http://lorempixel.com/' + $window.innerWidth + '/' + $window.innerHeight + '/people'
+			poster: '//lorempixel.com/1920/1080/people/3'
 		},
 		'/api/v1/member/andreas?fields=name,duties,description,poster': {
 			id: 'andreas',
@@ -103,7 +102,7 @@ angular.module('efg.mockService', [
 			},
 			duties: ['Lehre', 'Gebet'],
 			description: 'lorem ipsum dolor sit amet',
-			poster: 'http://lorempixel.com/' + $window.innerWidth + '/' + $window.innerHeight + '/people'
+			poster: '//lorempixel.com/1920/1080/people/9'
 		},
 		'/api/v1/member/daniel?fields=name,duties,description,poster': {
 			id: 'daniel',
@@ -113,7 +112,7 @@ angular.module('efg.mockService', [
 			},
 			duties: ['Gottesdienst'],
 			description: 'lorem ipsum dolor sit amet',
-			poster: 'http://lorempixel.com/' + $window.innerWidth + '/' + $window.innerHeight + '/people'
+			poster: '//lorempixel.com/1920/1080/people/10'
 		},
 		'/api/v1/member/jonas?fields=name,duties,description,poster': {
 			id: 'jonas',
@@ -123,12 +122,40 @@ angular.module('efg.mockService', [
 			},
 			duties: ['Jugendarbeit', 'Hauskreise'],
 			description: 'lorem ipsum dolor sit amet',
-			poster: 'http://lorempixel.com/' + $window.innerWidth + '/' + $window.innerHeight + '/people'
+			poster: '//lorempixel.com/1920/1080/people/6'
 		},
-		'/api/v1/event?limit=3': [
-			{id: 'weihnachtsgottesdienst', title: 'Weihnachtsgottesdienst', subtitle: '24. Dezember, 16:00 Uhr', img: 'img/christmas-tree.jpg'},
-			{id: 'allianzgebetswoche', title: 'Allianz Gebetswoche', subtitle: '11. bis 18. Januar', img: 'img/church-windows.jpg'},
-			{id: 'endzeitbibelabende', title: 'Endzeit-Bibelabende', subtitle: '27. Januar bis 10. Februar', img: 'img/beach-fire.jpg'}
+		'/api/v1/event?limit=3&fields=name,date,img': [
+			{
+				id: 'weihnachtsgottesdienst',
+				name: 'Weihnachtsgottesdienst',
+				date: [
+					(new Date('2014/12/24 16:00')).getTime()
+				],
+				img: '//lorempixel.com/500/400/abstract/1'
+			}, {
+				id: 'allianzgebetswoche',
+				name: 'Allianz Gebetswoche',
+				date: [
+					(new Date('2015/01/11 20:00')).getTime(),
+					(new Date('2015/01/12 20:00')).getTime(),
+					(new Date('2015/01/13 20:00')).getTime(),
+					(new Date('2015/01/14 20:00')).getTime(),
+					(new Date('2015/01/15 20:00')).getTime(),
+					(new Date('2015/01/16 20:00')).getTime(),
+					(new Date('2015/01/17 20:00')).getTime(),
+					(new Date('2015/01/18 20:00')).getTime()
+				],
+				img: '//lorempixel.com/500/400/abstract/2'
+			}, {
+				id: 'endzeitbibelabende',
+				name: 'Endzeit-Bibelabende',
+				date: [
+					(new Date('2015/01/27 20:00')).getTime(),
+					(new Date('2015/02/03 20:00')).getTime(),
+					(new Date('2015/02/10 20:00')).getTime()
+				],
+				img: '//lorempixel.com/500/400/abstract/3'
+			}
 		],
 		'/api/v1/next': [
 			{id: 'service', title: 'Gottesdienst', subtitle: 'Komm zu einem'},
