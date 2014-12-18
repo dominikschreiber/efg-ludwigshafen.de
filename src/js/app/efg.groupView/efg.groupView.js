da6f6fcd6a79c35b637e451e4e95ad4fc52b1d2e
Dominik Schreiber <domischreib@gmail.com>
2014-12-18 20:25:07 +0100

    added efg.groupView

---
+++ src/js/app/efg.groupView/efg.groupView.js
@@ -0,0 +1,20 @@
+angular.module('efg.groupView', [
+	'efg.mockService',
+	'efg.componentDirective',
+	'ng',
+	'ngRoute'
+])
+
+.config(function($routeProvider) {
+	$routeProvider.when('/group/:id', {
+		controller: 'GroupCtrl as group',
+		templateUrl: 'efg.groupView.tpl.html'
+	});
+})
+
+.controller('GroupCtrl', function(mock, $routeParams) {
+	mock.get('/api/v1/group/' + $routeParams.id + '?fields=name,poster').then(angular.bind(this, function success(result) {
+		this.title = result.name;
+		this.img = result.poster;
+	}));
+});
