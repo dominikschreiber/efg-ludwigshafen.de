fd67187367471d266e61720ed0ba5fe71cb708b2
Dominik Schreiber <domischreib@gmail.com>
2015-01-08 08:32:17 +0100

    removed uiGmapGoogleMapApiProvider configuration -- moved to efg.findusView

--- src/js/app/efg.js
+++ src/js/app/efg.js
@@ -1,24 +1,20 @@
+'use strict';
+
 angular.module('efg', [
 	'efg.indexView',
 	'efg.serviceView',
 	'efg.memberView',
 	'efg.findusView',
 	'efg.groupView',
-	'uiGmapgoogle-maps',
 	'ng',
 	'ngRoute'
 ])
 
-.config(function($routeProvider, $locationProvider, uiGmapGoogleMapApiProvider) {
+.config(function($routeProvider, $locationProvider) {
 	$routeProvider.otherwise({
 		redirectTo: '/'
 	});
 	
 	$locationProvider.html5Mode(false);
 	$locationProvider.hashPrefix('!');
-	
-	uiGmapGoogleMapApiProvider.configure({
-		v: '3.17',
-		libraries: 'weather,geometry,visualization'
-	});
 });
