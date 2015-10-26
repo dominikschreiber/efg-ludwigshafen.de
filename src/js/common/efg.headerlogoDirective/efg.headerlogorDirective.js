2e886265c5e7c762cf57987eb7b246e36001f59e
a_JAguar <weber.jonas@gmail.com>
2015-10-26 20:35:09 +0100

    adding missing files

---
+++ src/js/common/efg.headerlogoDirective/efg.headerlogorDirective.js
@@ -0,0 +1,15 @@
+angular.module('efg.headerlogoDirective', [
+	'ng'
+])
+
+.directive('headerlogo', function() {
+	return {
+		replace: true, // to allow style/ng-style
+		restrict: 'E',
+		scope: {
+			classes: '@'
+		},
+		templateUrl: 'efg.headerlogoDirective.tpl.html',
+		transclude: true
+	};
+});
