61a77660a904539c39963a5ef9518b2b94a8e4e3
Dominik Schreiber <domischreib@gmail.com>
2016-01-16 12:24:33 +0100

    add minimal sermon playback progress indicator to headerbar

--- src/js/common/efg.headerbarDirective/efg.headerbarDirective.js
+++ src/js/common/efg.headerbarDirective/efg.headerbarDirective.js
@@ -7,22 +7,18 @@ angular.module('efg.headerbarDirective', [
     'ng'
 ])
 
-.directive('headerbar', function(headerbar, player, sermon) {
+.directive('headerbar', function(headerbar, player, sermon, $window, $log) {
     return {
         templateUrl: 'efg.headerbarDirective.tpl.html',
         link: function($scope) {
             var initial = true;
 
-            $scope.$watch(function() { return headerbar.title(); }, function(title) {
-                if (title !== undefined) {
-                    $scope.header = title;
-                }
-            });
             $scope.$watch(headerbar.query, function(items) {
                 if (items) {
                     $scope.leftitems = items;
                 }
             });
+
             $scope.$watch(player.isPaused, function(paused) {
                 $scope.playpause = {
                     id: 'playpause',
@@ -31,6 +27,18 @@ angular.module('efg.headerbarDirective', [
                     type: 'button'
                 };
             });
+
+            $scope.position = 0;
+            $scope.$on('player:timeupdate', function(event, position) {
+                $log.log(position, $scope.fullwidth);
+                $scope.position = position * $scope.fullwidth;
+            });
+
+            $scope.fullwidth = $window.innerWidth;
+            $scope.$watch(function() { return $window.innerWidth; }, function(width) {
+                $scope.fullwidth = width;
+            });
+
             $scope.$watch(sermon.get, function(s) {
                 if (s) {
                     $scope.sermon = s;
@@ -42,7 +50,8 @@ angular.module('efg.headerbarDirective', [
                 }
             });
         },
-        transclude: true
+        transclude: true,
+        replace: true
     };
 })
 
--- src/js/common/efg.playerService/efg.playerService.js
+++ src/js/common/efg.playerService/efg.playerService.js
@@ -5,14 +5,24 @@ angular.module('efg.playerService', [
     'ng'
 ])
 
-.factory('player', function(sermon) {
+.factory('player', function(sermon, $rootScope, $log) {
     var audio = document.createElement('audio');
 
     document.body.appendChild(audio);
 
+    audio.addEventListener('timeupdate', function(t) {
+        $rootScope.$apply(function() {
+            $rootScope.$broadcast('player:timeupdate', t.target.currentTime / t.target.duration);
+        });
+    });
+
     return {
         toggle: function() {
-            audio.paused ? audio.play() : audio.pause();
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
         },
         play: function() {
             audio.play();
@@ -20,10 +30,14 @@ angular.module('efg.playerService', [
         isPaused: function() {
             return audio.paused;
         },
+        position: function(position) {
+            if (position) {
+                audio.currentTime = audio.duration * position;
+            }
+            return audio.currentTime / audio.duration;
+        },
         reload: function() {
-            var current = sermon.get();
-
-            audio.src = current.source.src;
+            audio.src = sermon.get().source.src;
         }
-    }
+    };
 });
