29e753a6ed3920e4bc58486555468be72538b78c
Dominik Schreiber <domischreib@gmail.com>
2018-03-23 10:03:31 +0100

    fix thumbnail-url for firefox

--- src/js/common/bootstrap.thumbnailDirective/bootstrap.thumbnailDirective.js
+++ src/js/common/bootstrap.thumbnailDirective/bootstrap.thumbnailDirective.js
@@ -1,26 +1,26 @@
-'use strict';
+'use strict'
 
 angular
-  .module('bootstrap.thumbnailDirective', ['ng'])
-  .directive('thumbnail', function() {
-    return {
-      link: function($scope, $element, $attributes) {
-        $scope.contains = function(haystack, needle) {
-          return haystack.indexOf(needle) > -1;
-        };
-      },
-      replace: true,
-      restrict: 'E',
-      scope: {
-        href: '=',
-        header: '=',
-        subheader: '=',
-        // if img contains a whitespace (thus: no valid url)
-        // it is assumed to be a classList for icons like
-        // 'glyphicon glyphicon-envelope'
-        img: '=',
-        imgclass: '='
-      },
-      templateUrl: 'bootstrap.thumbnailDirective.tpl.html'
-    };
-  });
+	.module('bootstrap.thumbnailDirective', ['ng'])
+	.directive('thumbnail', function() {
+		return {
+			link: function($scope, $element, $attributes) {
+				$scope.contains = function(haystack, needle) {
+					return haystack.indexOf(needle) > -1
+				}
+			},
+			replace: true,
+			restrict: 'E',
+			scope: {
+				xhref: '&',
+				header: '=',
+				subheader: '=',
+				// if img contains a whitespace (thus: no valid url)
+				// it is assumed to be a classList for icons like
+				// 'glyphicon glyphicon-envelope'
+				img: '=',
+				imgclass: '='
+			},
+			templateUrl: 'bootstrap.thumbnailDirective.tpl.html'
+		}
+	})
