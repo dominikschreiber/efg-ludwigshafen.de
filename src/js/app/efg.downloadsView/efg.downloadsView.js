ce5458e9bb05adc014956fe3a3959471d3e08a8c
Dominik Schreiber <dominik.schreiber@sap.com>
2020-10-06 17:47:36 +0200

    add Handout for last sermon

--- src/js/app/efg.downloadsView/efg.downloadsView.js
+++ src/js/app/efg.downloadsView/efg.downloadsView.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular
-  .module('efg.downloadsView', ['efg.downloadsApi', 'ng', 'ngRoute'])
+  .module('efg.downloadsView', ['ng', 'ngRoute'])
   .config(function($routeProvider) {
     $routeProvider.when('/downloads', {
       controller: 'DownloadsCtrl as downloads',
       templateUrl: 'efg.downloadsView.tpl.html'
     });
   })
-  .controller('DownloadsCtrl', function(downloadsApi) {
-    downloadsApi.query().then(
-      function(results) {
-        this.items = results;
-      }.bind(this)
-    );
+  .controller('DownloadsCtrl', function() {
+    this.items = [{
+      url: '/assets/downloads/2020-10-04 Arbeitsanweisung Dankbarkeit.pdf',
+      title: '2020-10-04 Arbeitsanweisung Dankbarkeit',
+      ending: 'pdf'
+    }];
 
     this.getImageSrc = function(asset) {
       return /(png|jpe?g)$/.test(asset.ending)
