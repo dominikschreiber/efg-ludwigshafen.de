4177d184318039be62fdfc9191fd72aa3ca17664
Dominik Schreiber <dominik.schreiber@sap.com>
2020-12-02 09:36:10 +0100

    delete angular-based app

--- src/js/app/efg.dataProtectionView/efg.dataProtectionView.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular
-  .module('efg.dataProtectionView', ['efg.componentDirective', 'ng', 'ngRoute'])
-  .config(function($routeProvider) {
-    $routeProvider.when('/data-protection', {
-      templateUrl: 'efg.dataProtectionView.tpl.html'
-    });
-  });
--- src/js/app/efg.imprintView/efg.imprintView.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular
-  .module('efg.imprintView', ['efg.componentDirective', 'ng', 'ngRoute'])
-  .config(function($routeProvider) {
-    $routeProvider.when('/imprint', {
-      templateUrl: 'efg.imprintView.tpl.html'
-    });
-  });
